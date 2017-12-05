b9f81105a39df637d3481a0f299f0f45722c2a9d
solarashes <6366037+solarashes@users.noreply.github.com>
2017-12-05 16:03:28 -0500

    add components

---
+++ app/components/Help.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+let Help = React.createClass({
+  render: function() {
+    return (
+      <div className="container-fluid">
+        <h1>{this.props.title}</h1>
+        <section className="text-center">
+          <h4>Please see the <a href="https://lnchpad.info/deployment.html" target="_blank">FAQ</a> on the <a href="https://lnchpad.info/deployment.html" target="_blank">LaunchPAD</a> website.</h4>
+          <p>dApp documentation coming soon.</p>
+        </section>
+      </div>
+    );
+  }
+});
+
+module.exports = Help;
