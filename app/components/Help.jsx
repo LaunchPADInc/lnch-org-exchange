1878c126cdf8126562be88cb3035e70958aca302
Admin <solarashes>
2017-12-13 18:46:56 -0500

    update index.html

--- app/components/Help.jsx
+++ app/components/Help.jsx
@@ -6,7 +6,7 @@ let Help = React.createClass({
       <div className="container-fluid">
         <h1>{this.props.title}</h1>
         <section className="text-center">
-          <h4>Please see the <a href="https://lnchpad.info/deployment.html" target="_blank">FAQ</a> on the <a href="https://lnchpad.info/deployment.html" target="_blank">LaunchPAD</a> website.</h4>
+          <h4>Please see the <a href="https://lnchpad.com/deployment.html" target="_blank">FAQ</a> on the <a href="https://lnchpad.com/deployment.html" target="_blank">LaunchPAD</a> website.</h4>
           <p>dApp documentation coming soon.</p>
         </section>
       </div>
