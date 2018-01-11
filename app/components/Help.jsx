b94be62cba4ca91dc65899d13a15894ff23f180b
Admin <solarashes>
2018-01-10 22:52:22 -0500

    add Help.js

--- app/components/Help.jsx
+++ app/components/Help.jsx
@@ -6,8 +6,8 @@ let Help = React.createClass({
       <div className="container-fluid">
         <h1>{this.props.title}</h1>
         <section className="text-center">
-          <h4>Please see the <a href="https://lnchpad.com/deployment.html" target="_blank">FAQ</a> on the <a href="https://lnchpad.com/deployment.html" target="_blank">LaunchPAD</a> website.</h4>
-          <p>dApp documentation coming soon.</p>
+          <h4>Please see the <a href="http://lnchpad.com/deployment/" target="_blank">FAQ</a> on the <a href="http://lnchpad.com/deployment/" target="_blank">LaunchPAD</a> website.</h4>
+          <p>dApp documentation coming soon on the <a href="https://github.com/LaunchPADInc" target="_blank">LaunchPAD GitHub</a>.</p>
         </section>
       </div>
     );
